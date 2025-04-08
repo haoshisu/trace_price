5d001776f35b26a4dabbc74c280eb083f4dd9cd4
haoshisu <haoshisu0614@gmail.com>
2025-04-08 23:14:44 +0800

    move architecture

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,12 @@
 import mongooseDB from 'mongoose'
+import dotenv from 'dotenv'
 
-const password = import.meta.env.VITE_MONGODB_PASSWORD
+dotenv.config()
 
-const uri = `mongodb+srv://haoshisu0614:${password}@cluster0.o5zlror.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const password = process.env.MONGODB_PASSWORD 
+const account = process.env.MONGODB_ACCOUNT
+
+const uri = `mongodb+srv://${account}:${password}@cluster0.o5zlror.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 mongooseDB.connect(uri).then(() => console.log("連接成功")).catch(() => console.log("連接失敗"))
 
 export default mongooseDB
