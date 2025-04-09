aeab13a237f8d07f76f23a47aa0a09ac9e4b1bc0
haoshisu <haoshisu0614@gmail.com>
2025-04-09 15:32:18 +0800

    modify puppeteer

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const password = process.env.MONGODB_PASSWORD 
-const account = process.env.MONGODB_ACCOUNT
 
 const uri = `mongodb+srv://haoshisu0614:${password}@cluster0.o5zlror.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 mongooseDB.connect(uri).then(() => console.log("連接成功")).catch(() => console.log("連接失敗"))
