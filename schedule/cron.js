c71b70227f2819d744b8f9fe37ed2ccf76c774bd
haoshisu <haoshisu0614@gmail.com>
2025-04-10 09:58:55 +0800

    modify cron.js

--- schedule/cron.js
+++ schedule/cron.js
@@ -1,16 +1,28 @@
-import cron from 'node-cron'
+import mongoose from 'mongoose'
+import scrapeProduct from '../backend/puppeteer'
+import Product from '../backend/modal/productSchema'
+import dotenv from 'dotenv'
 
-// 定時爬取
-cron.schedule('0 6 * * *',async () => {
-    const products = await Product.find()
-    if(products.length === 0 ) return //無資料直接return 
-    const now  = new Date().toISOString().slice(0,10)
-    for(const p of products){
-        const result  = await scrapeProduct(p.url)
-        const newHistory = {date:now,price:result.price}
+dotenv.config()
 
-        p.history.unshift(newHistory)
-        await p.save()
-    }
-    console.log(`${now}更新完成`)
+// 連線 MongoDB
+await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+
+const products = await Product.find()
+if (products.length === 0) {
+    console.log('沒有任何產品')
+    process.exit(0)
+}
+
+const now = new Date().toISOString().slice(0, 10)
+for (const p of products) {
+    const result = await scrapeProduct(p.url)
+    const newHistory = { date: now, price: result.price }
+    p.history.unshift(newHistory)
+    await p.save()
+}
+console.log(`${now} 更新完成`)
+process.exit(0)
